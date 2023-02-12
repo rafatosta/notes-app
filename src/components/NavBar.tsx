f32175a1a21885221795dc7393a27a92794e4be0
Rafael Tosta <rafa.ecomp@gmail.com>
2023-02-12 19:39:44 -0300

    :construction: fix(NavBar) Ajuste na responsividade do componente

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 function NavBar() {
   return (
-    <nav className="fixed top-0 w-full h-16 flex flex-row justify-between items-center bg-white p-4 shadow-md">
-      <p className="text-2xl font-semibold text-gray-800">Minhas notas</p>
+    <nav className="fixed top-0 w-full h-16 flex flex-row justify-between items-center bg-white p-4 shadow-md gap-x-4">
+      <p className="text-sm text-center lg:text-2xl font-semibold text-gray-800">Minhas notas</p>
       <button className="border border-gray-400 rounded-lg bg-gray-200 text-gray-700 p-2 px-4 w-[400px] text-left drop-shadow-md cursor-text">
         Criar uma nota...
       </button>
-      <div>Sobre</div>
+      <div className="hidden lg:flex">Sobre</div>
     </nav>
   );
 }
