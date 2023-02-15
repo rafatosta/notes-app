6e2ce39f5fd376d906992792e38ebff96d81725d
Rafael Tosta <rafa.ecomp@gmail.com>
2023-02-15 16:26:59 -0300

    :recycle: refactor(NavBar) Melhoria no visual

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -45,17 +45,67 @@ function NavBar() {
   }
 
   return (
-    <nav className="fixed top-0 w-full h-16 flex flex-row justify-between items-center bg-white p-4 shadow-md gap-x-4">
-      <p className="text-sm text-center lg:text-2xl font-semibold text-gray-800">
-        Minhas notas
-      </p>
+    <nav className="bg-[#24292f] text-white fixed top-0 w-full h-16 flex flex-row justify-around items-center p-4 shadow-md">
+      <div className="flex justify-start items-center gap-2 w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-8 h-8 "
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+
+        <p className="hidden sm:flex text-sm sm:text-2xl font-semibold">
+          Minhas notas
+        </p>
+      </div>
+
       <button
-        className="border border-gray-400 rounded-lg bg-gray-200 text-gray-700 p-2 px-4 w-[400px] text-left drop-shadow-md cursor-text"
+        className="w-full flex flex-row items-center justify-center gap-2 rounded-md bg-[#2da44e] hover:bg-[#308d4a] active:bg-[#2a6b3c] text-white font-semibold p-2 px-8"
         onClick={openModal}
       >
-        Criar uma nota...
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+          />
+        </svg>
+        <p> Nova nota</p>
       </button>
-      <div className="hidden lg:flex">Sobre</div>
+
+      <div className="hidden lg:flex w-full lg:justify-end">
+        <button className=" hover:text-[#308d4a] active:text-[#2a6b3c]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+            />
+          </svg>
+        </button>
+      </div>
 
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
